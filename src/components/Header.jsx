ee5659d6c36609961d739f239d48ecaf4e624d51
Fernando lopez <ferchholop18@hotmail.com>
2023-12-20 15:48:53 -0500

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="header">
         <h1>Weather React App </h1>
-        <a href="https://www.google.com/?hl=es" target="_blank"><i className="fa-brands fa-github"></i></a>
+        <a href="https://github.com/AlvaroFernandoLopez/weather" target="_blank"><i className="fa-brands fa-github"></i></a>
        
     </header>
   )
